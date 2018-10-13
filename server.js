a80b95fac7015f0fe113d081aa76fad080b60eb7
Lina Lockheart <angelinalockheart@gmail.com>
2018-10-13 12:34:50 -0400

    added API.js in utils folder and installed axios on the client side

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ if (process.env.NODE_ENV === "production") {
 // Define API routes here
 require("./routes/apiRoutes")(app);
 
+router.use("/api", apiRoutes);
+
 // Send every other request to the React app
 
 // Define any API routes before this runs
