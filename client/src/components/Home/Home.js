148beaa0af62ec686efcb644cd703c8e89934600
Jdelorim <joshuadelorimier@gmail.com>
2018-10-15 20:58:47 -0400

    testing git

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,7 +6,7 @@ import API from "../../utils/API";
 class Home extends Component {
     render() {
         return(
-            <div><img src="https://media.giphy.com/media/TM8D3N62ARWfu/giphy-facebook_s.jpg" alt="whatever"/></div>
+            <div></div>
         )
     }
 }
